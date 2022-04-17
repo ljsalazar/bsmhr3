c76a85c69c400562042501e49e9dd59f4bcfda7d
ljsalazar <ljdotesalazar1801@gmail.com>
2022-04-17 23:05:36 +0800

    Edited Claim_index and Payable Option
    -Format application for Payable leave request as claims
    -void for User level 3 in Action column  claim_index.php

--- hr3/payable_option.php
+++ hr3/payable_option.php
@@ -1,10 +1,15 @@
-<?php
-	$page_title = 'Admin | Payable Leave Details ';
+1<?php
+	$page_title = 'Request Claims';
 	require_once('includes/load.php');
 	// Checkin What level user has permission to view this page
 	page_require_level(2);
 	
+	$user = current_user();
+	$user_level = $user['user_level'];
+	?><?php
+	require_once("includes/load.php");
 	if (!$session->isUserLoggedIn(true)) { redirect('index.php', false);}
+	
 	$claim;$currency;$amount;
 	$conn = new mysqli('localhost', 'root', '', 'bank') or die(mysqli_error());       
 	$user = current_user();
@@ -12,145 +17,123 @@
 	$user_id = $user['id'];
 	$user_level = $user['user_level'];
 	$fullname = $user['name'];
-?>
-
-<?php include_once('layouts/header.php'); ?>
-<!-- This will be the body -->
-<div class="row">
-	<div class="col-md-12">
-		<?php echo display_msg($msg); ?>
-		<?php 
-			$query = $conn->query("SELECT claim_notif FROM users WHERE id='$user_id'");
-			while($user_data = mysqli_fetch_array($query)) {
-				$claim_notif = $user_data['claim_notif'];
-			}
-		?>
-		<?php if ($user_level <= '2'): ?>
-		<nav class="breadcrumbs">
-			<a href="claim_type.php" class="breadcrumbs__item">Types of Claims</a>
-			<a href="claim_history.php" class="breadcrumbs__item">Claims History <?php if(!$claim_notif==0){ ?><span class="badge" style="background-color: red;"><?php echo (int)$claim_notif; ?></span><?php } ?></a>
-			<a href="claim_index.php" class="breadcrumbs__item is-active">Request Claims</a>
-		</nav>
-		
-		<?php else: ?>
-		<nav class="breadcrumbs">
-			<a href="claim_type.php" class="breadcrumbs__item">Types of Claims</a>
-			<a href="claim_history.php" class="breadcrumbs__item is-active">Claims History <?php if(!$claim_notif==0){ ?><span class="badge" style="background-color: red;"><?php echo (int)$claim_notif; ?></span><?php } ?></a>
-		</nav>	
-		<?php endif;?>
-	</div>
-	
-	<div class="col-md-12">
-		<div class="card h-100">
-			<div class="card-header">
-				<h2>Payable Leave Details:</h2>
-				<table class="table table-bordered table-hover" style="width:100%">
-					<tbody>
-						<?php 
-							
-							//Query Statement for leave history===
-							$conn = new mysqli('localhost', 'root', '', 'bank') or die(mysqli_error());
-							$user = current_user();
-							$userid = (int)$user['id'];
-							//Getting an ID located in the URL...
-							$lid=intval($_GET['id']);
-							//===================================
-							$sql  =" SELECT l.id,l.LeaveType,l.FromDate,l.ToDate,l.Description,l.PostingDate,l.AdminRemarkDate,l.AdminRemark,l.Status,l.empid,u.name,u.username";
-							$sql .=" FROM tblleaves l";
-							$sql .=" LEFT JOIN users u ON l.empid = u.id";
-							$sql .=" WHERE l.id = '{$lid}'";
-							$sql .=" ORDER BY id DESC";
-							if($result = $conn->query($sql)){
-								while ($row = $result -> fetch_row()) {
-									
-								?>
-								<tr>
-									<td style="font-size:16px;"> <b>Name:</b></td>
-									<td><?php echo remove_junk($row[10]); ?></td>
-									<td style="font-size:16px;"><b>Id:</b></td>
-									<td colspan="3"><?php echo remove_junk($row[9]); ?></td>
-									<!-- <td style="font-size:16px;"><b>Gender :</b></td>
-									<td><?php echo htmlentities($result->Gender);?></td> -->
-								</tr>
+?><?php include_once('layouts/header.php'); ?>
+<html>
+	<head>
+		<meta name="generator"
+		content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
+		<title>Request Claims</title>
+	</head>
+	<body>
+		<div class="row">
+			<div class="col-md-12"><?php echo display_msg($msg); ?>
+				<?php 
+					$query = $conn->query("SELECT claim_notif FROM users WHERE id='$user_id'");
+					while($user_data = mysqli_fetch_array($query)) {
+						$claim_notif = $user_data['claim_notif'];
+					}
+				?>
+				<?php if ($user_level <= '2'): ?>
+				<nav class="breadcrumbs">
+					<a href="claim_type.php" class="breadcrumbs__item">Types of Claims</a>
+					<a href="claim_history.php" class="breadcrumbs__item">Claims History <?php if(!$claim_notif==0){ ?><span class="badge" style="background-color: red;"><?php echo (int)$claim_notif; ?></span><?php } ?></a>
+					<a href="claim_index.php" class="breadcrumbs__item is-active">Request Claims</a>
+				</nav>
+				
+				<?php else: ?>
+				<nav class="breadcrumbs">
+					<a href="claim_type.php" class="breadcrumbs__item">Types of Claims</a>
+					<a href="claim_history.php" class="breadcrumbs__item is-active">Claims History <?php if(!$claim_notif==0){ ?><span class="badge" style="background-color: red;"><?php echo (int)$claim_notif; ?></span><?php } ?></a>
+				</nav>	
+				<?php endif;?>
+				
+				<div class="col-md-12">
+					<div class="card h-100">
+						<div class="card-header">
+							<h2>Request Claims</h2>
+							<p>Provided as for Payable Leaves</p>
+							</div><?php
+							if(isset($_POST['add_claim'])) {
+								$claim = $_POST['claim'];
+								$user_selected = $_POST['user_selected'];
+								$status = "Pending";
+								$date = date("Y-m-d", strtotime("+0 HOURS"));
 								
-								<tr>
-									<td style="font-size:16px;"><b>Username:</b></td>
-									<td colspan="5"><?php echo remove_junk($row[11]); ?></td>
-									<!-- <td style="font-size:16px;"><b>Emp Contact No. :</b></td>
-									<td><?php echo htmlentities($result->Phonenumber);?></td>-->
-								</tr>
+								#$q_student = $conn->query("SELECT * FROM `users` WHERE `name` = '$user_selected'") or die(mysqli_error());
+								#$f_student = $q_student->fetch_array();
 								
-								<tr>
-									<td style="font-size:16px;"><b>Leave Type :</b></td>
-									<td><?php echo remove_junk($row[1]) ?></td>
-									<td style="font-size:16px;"><b>Leave Date:</b></td>
-									<td><b>From</b> <?php echo remove_junk($row[2]);?> <b>To</b> <?php echo remove_junk($row[3]);?></td>
-									<td style="font-size:16px;"><b>Posting Date:</b></td>
-									<td><?php echo remove_junk($row[5]);?></td>
-								</tr>
-								
-								<tr>
-									<td style="font-size:16px;"><b>Leave Description: </b></td>
-									<td colspan="5"><?php echo remove_junk($row[4]);?></td>                          
-								</tr>
-								
-								<tr>
-									<td style="font-size:16px;"><b>Leave Status:</b></td>
-									<td colspan="5"><?php $stats=$row[8];
-										if($stats==1){
-										?>
-										<span style="color: green">Approved</span>
-										<?php } if($stats==2)  { ?>
-										<span style="color: red">Not Approved</span>
-										<?php } if($stats==0)  { ?>
-										<span style="color: blue">waiting for approval</span>
-									<?php } ?>
-									</td>
-								</tr>
-								
-								<tr>
-									<td style="font-size:16px;"><b>Admin Remark: </b></td>
-									<td colspan="5"><?php
-										if($row[7]==""){
-											echo "waiting for Approval";  
-										}
-										else{
-											echo remove_junk($row[7]);
-										}
-									?></td>
-								</tr>
-								
-								<tr>
-									<td style="font-size:16px;"><b>Admin Action taken date : </b></td>
-									<td colspan="5"><?php
-										if($row[6]==""){
-											echo "NA";  
-										}
-										else{
-											echo remove_junk($row[6]);
-										}
-									?></td>
-								</tr>
-								<!-- //Condition: If Admin taken action or not... -->
+								$query = $conn->query("SELECT * FROM users WHERE name = '$user_selected'");
+								while($user_data = mysqli_fetch_array($query)) {
+									$add_user_id = $user_data['id'];
+									$add_username = $user_data['username'];
+									$add_user_level = $user_data['user_level'];
+									$add_fullname = $user_data['name'];
+								}
+								$conn->query("INSERT INTO `claim` VALUES('', '$claim', '$date', '$status', '0', '$add_user_id', '$add_username', '$add_user_level', '$add_fullname')") or die(mysqli_error());
+								$session->msg('s',"Claim Request Successfully Added");
+								echo "<script>window.location.href='claim_index.php';</script>";
+							}
+						?>
+						<div class="panel-body" style="margin:50px">
+							<form method="post" action="claim_index.php">
+								<p>Selected Claims: </p>
+								<div class="form-group">
 								<?php 
-									if($stats==0)
-									{
-										
-									?>
-									<tr>
-										<td colspan="5">
-											<a href="leave_approval_form.php?id=<?php echo remove_junk($row[0]);?>" class="btn" style="margin-bottom:10px; background-color:steelblue; color: whitesmoke;"> Take Action</a>
-											
-										</td>
-									</tr>
-								<?php } ?>
-								<!-- // -->
-							<?php } }?>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</div>
-</div>
+													
+													//Query Statement for leave history
+													$conn = new mysqli('localhost', 'root', '', 'bank') or die(mysqli_error());
+													$user = current_user();
+													$userid = (int)$user['id'];
+													$id = $_GET['id'];
+													
+													if ($user_level <= 2){
+														$sql  =" SELECT l.id,l.LeaveType,l.FromDate,l.ToDate,l.Description,l.PostingDate,l.AdminRemarkDate,l.AdminRemark,l.Status,l.empid,l.amount_of_days,l.remaining_days,u.name";
+														$sql .=" FROM tblleaves l";
+														$sql .=" LEFT JOIN users u ON l.empid = u.id";
+														$sql .=" WHERE l.Status = 1 AND l.id ='{$id}' ";
+														// $sql .=" WHERE l.empid= '{$userid}'";
+														$sql .=" ORDER BY id DESC";
+													} else {
+														$sql  =" SELECT l.id,l.LeaveType,l.FromDate,l.ToDate,l.Description,l.PostingDate,l.AdminRemarkDate,l.AdminRemark,l.Status,l.empid,l.amount_of_days,l.remaining_days,u.name";
+														$sql .=" FROM tblleaves l";
+														$sql .=" LEFT JOIN users u ON l.empid = u.id";
+														$sql .=" WHERE l.Status = 1 AND empid='{$userid}' AND l.id ='{$id}' ";
+														$sql .=" ORDER BY id DESC";
+													}
+													// if($result = $conn->query($sql)){
+													// 	while ($row = $result -> fetch_row()) {
+														$result = $conn->query($sql);
+														$row = $result->fetch_row();
+														?>
+									<input type="text" class="form-control" name="claim" value="<?php echo "Payable Leaves( ".($row[1]).")"; ?>" readonly>
+									
+								</div>
+								</br>
+								<div class="form-group">
+									<p>Selected User:</p>
+									<input type="text" class="form-control" name="user_selected" value="<?php echo $row[12]; ?>" readonly>
 
-<?php include_once('layouts/footer.php'); ?>							
+								</div>
+								</br>
+								<button type="submit" name="add_claim" class="btn btn-primary" value="add_claim">Add</button>
+								<a href="claim_index.php" class="btn btn-danger">Cancel</a>
+								</br></br></br>
+								
+							</form>
+						</div>
+					</div>
+				</div>
+				</div><?php include_once('layouts/footer.php'); ?>
+			
+			
+			<!--from startbootstrap.com this is for Datatables...
+				<link href="dist/css/styles.css" rel="stylesheet" />
+			-->
+			<?php include('layouts/table/tablefooter.php');?>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+			<script src="dist/js/scripts.js"></script>
+			<script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+			<script src="dist/js/datatables-simple-demo.js"></script>
+		</body>
+	</html>
